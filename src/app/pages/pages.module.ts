b5b1526d09013093caddf5e20fe828cc5f5c1802
root <root@carpintero.example.com>
2018-12-01 22:32:02 -0500

    Input, output y viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from "@angular/forms";
+
+// ng2 Charts
+
+import { ChartsModule } from "ng2-charts";
+
 import { PagesComponent }  from './pages.component';
 
 import { PAGES_ROUTES } from './pages.routes';
@@ -10,17 +16,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+//Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
-    ],
-
-    imports: [ 
-        SharedModule,
-        PAGES_ROUTES 
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
 
     exports: [
@@ -30,6 +38,11 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         Graficas1Component
     ],
 
-    providers: [],
+    imports: [ 
+        SharedModule,
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
+    ],
 })
 export class PagesModule {}
